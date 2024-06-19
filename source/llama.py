8f0dfa76368cadb9b14bd9f5aee7c050e49a02ff
Murilo Martin <murilodsmartin@gmail.com>
2024-06-18 23:59:25 -0300

    Started testing

--- source/llama.py
+++ source/llama.py
@@ -1,16 +1,31 @@
 import ollama
 from tqdm.auto import tqdm
-
+from prompt import prompt_factory, strategys
 client = ollama.Client(host='http://localhost:11434')
 
 
 
 if __name__ == '__main__':
 
-    response = ollama.chat(model='mistral', messages=[
-    {
-        'role': 'user',
-        'content': 'Why is the sky blue?',
-    },
-    ])
-    print(response['message']['content'])
+    with open('DataSets\Promise_exp\PROMISE_exp_sample_treated.txt', 'r') as file:
+        lines = file.readlines()
+        #for strategy in strategys:
+    responses = []
+    line_counter = 1
+    for i in lines:
+        req = i.strip()
+        user_prompt = prompt_factory(strategy='zero_shot', requirement=req)['user_msg']
+        response = client.chat(model='llama3', messages=[
+            {
+                'role': 'user',
+                'content': f'{user_prompt}',
+            },
+        ])
+        responses.append(response['message']['content'])
+        file_name = r'DataSets\Promise_exp\results\zero_shot_llama3.csv'
+    
+    with open(file_name, 'w') as file:
+        for i in range(len(responses)):
+            file.write(f'{line_counter},{responses[i]}\n')
+            
+        
--- source/prompt.py
+++ source/prompt.py
@@ -18,38 +18,42 @@ def prompt_factory(requirement: str, strategy: str) -> dict:
     user_msg = ''
 
     if strategy == 'zero_shot':
-        user_msg = f"For the given requirement: {requirement} label it as Usability, Security,Performance, or Maintainability."
+        user_msg = f"For the given requirement: {requirement} label it as a functional (FR) or non functional requirement (NFR). Answer must be only the label. Do not add any additional information."
     
     if strategy == 'few_shot_cot':
-        req = 'Every user of the system shall be authenticated and authorized.'
-        user_msg = f"user_1= For the given requirement: {req}  label it as Usability, Security,Performance, or Maintainability. assistent_1=This requirement falls under the category of Security It specifically addresses the aspect of ensuring the integrity of the system's data by preventing incorrect data from being introduced, which is crucial for maintaining data accuracy and reliability, thereby safeguarding against potential security threats such as data corruption or manipulatio user_2= For the given requirement: {requirement} label it as Usability, Security,Performance, or Maintainability."
+        req = '1 - Every user of the system shall be authenticated and authorized.'
+        user_msg = f"user_1= For the given requirement: {req}  label it as a functional (FR) or non functional requirement (NFR). Answer in the following format: Number , Label \
+        assistent_1=This requirement falls under the category of NFR. It specifically addresses the aspect of ensuring the integrity of the system's data by preventing incorrect data from being introduced, which is crucial for maintaining data accuracy and reliability, thereby safeguarding against potential security threats such as data corruption or manipulation. \
+        answer_1 = 1,FNR \
+        user_2= For the given requirement: {requirement} label it as a functional (FR) or non functional requirement (NFR). Answer in the following format: Number , Label"
         
     
     if strategy =='zero_shot_cot':
-        user_msg = f"For the given requirement: {requirement} label it as Usability, Security,Performance, or Maintainability. answer:Lets think step by step,"
+        user_msg = f"For the given requirement: {requirement} label it as a functional (FR) or non functional requirement (NFR). Answer in the following format: Number , Label. Lets think step by step"
         
     if strategy == 'raw_inst':
-        user_msg = f"You are an expert in requirements engineering.  You are tasked with with the classification of non functinal  requierments for a software project. You should consider 4 types  of non functional requirements: Usability, Security,Performance,  and Maintainability Usability involves user interaction ease and satisfaction. Security ensures data protection and system integrity. Performance  addresses system responsiveness and scalability. Maintainability focuses on system modification ease and cost reduction.  For the given requierment: {requirement}  label it as Usability, Security,Performance, and Maintainability."
-    if strategy == 'sys_inst':
-        sys_msg = "You are an expert in requirements engineering. You are tasked with with the classification of non functinal requierments for a software project. You should consider 4 types of non functional requirements: Usability, Security,Performance, and Maintainab "
-        user_msg = f"For the given requierment: {requirement} label it as Usability, Security,Performance, and Maintainability."
+        user_msg = f"You are an expert in requirements engineering.  You are tasked with with the classification of non functinal  requierments for a software project. You should consider 2 types  of  requirements: functional (FR) and non functional (NFR) requirements. Functional requirements specify what a system should do, detailing the necessary tasks, behaviors, and functions, while non-functional requirements define the system's quality attributes, such as performance, usability, reliability, and security, ensuring the system's operational standards are met. \
+        For the given requierment: {requirement}  label it as a functional (FR) or non functional requirement (NFR). Answer in the following format: Number , Label"
+
+    # if strategy == 'sys_inst':
+    #     sys_msg = "You are an expert in requirements engineering. You are tasked with with the classification of non functinal requierments for a software project. You should consider  2 types  of  requirements: functional (FR) and non functional (NFR) requirements. Answer in the following format: Number , Label"
+    #     user_msg = f"For the given requierment: {requirement} label it as a functional (FR) or non functional requirement (NFR)."
         
-    if strategy == 'both_inst':
-        sys_msg = "You are an expert in requirements engineering. You are tasked with with the classification of non functinal requierments for a software project. You should consider 4 types of non functional requirements: Usability, Security,Performance, and Maintainability"
-        user_msg = f"Usability involves user interaction ease and satisfaction. Security ensures data protection and system integrity. Performance  addresses system responsiveness and scalability. Maintainability focuses on system modification ease and cost reduction. For the given requierment: {requirement} label it as Usability, Security,Performance, and Maintainability. "
+    # if strategy == 'both_inst':
+    #     sys_msg = "You are an expert in requirements engineering. You are tasked with with the classification of non functinal requierments for a software project. You should consider  2 types  of  requirements: functional (FR) and non functional (NFR) requirements."
+    #     user_msg = f"Functional requirements specify what a system should do, detailing the necessary tasks, behaviors, and functions, while non-functional requirements define the system's quality attributes, such as performance, usability, reliability, and security, ensuring the system's operational standards are met. For the given requierment: {requirement} label it as a functional (FR) or non functional requirement (NFR). "
  
         
 
         
     return {'sys_msg': sys_msg, 'user_msg':user_msg}
         
-        
+strategys = ['zero_shot', 'few_shot_cot', 'zero_shot_cot', 'raw_inst']       
     
 
 if __name__ == '__main__':
     req = 'The system shall refresh the display every 60 seconds.'
-    strategys = ['zero_shot', 'few_shot_cot', 'zero_shot_cot', 'raw_inst',
-                        'sys_inst', 'both_inst']
+    strategys = ['zero_shot', 'few_shot_cot', 'zero_shot_cot', 'raw_inst']
     responses = []
     for strategy in strategys:
         responses.append(prompt_factory(req,strategy))
