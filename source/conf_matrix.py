829ce24bdd2742bfd36d59c051819ddf865625f4
Murilo Martin <murilodsmartin@gmail.com>
2024-12-01 13:12:58 -0300

    Changes

--- source/conf_matrix.py
+++ source/conf_matrix.py
@@ -33,44 +33,55 @@ def plot_confusion_matrix(cm, report, strategy , labels):
     plt.text(0.5, 0.5, report, ha='center', va='center', fontsize=12)
     plt.show()
 
-def confusion_matrix_for_models(file_path, labels, model_name ):
-    with open(f'{file_path}', encoding='utf-8') as f:
-        data = json.load(f)
-    df = pd.DataFrame(data)
-    #print(df['response'])
+def get_data(models):
+    consolidated_df = pd.DataFrame()
+    for model in models:
+        path = rf'C:\Users\Murilo\Desktop\Projetos\TCC\results\{model}.json'
+        with open(path, encoding='utf-8') as f:
+            data = json.load(f)
+        df = pd.DataFrame(data)
+        df['model'] = model
+        consolidated_df = pd.concat([consolidated_df, df], ignore_index=True)
+
     for strategy in strategys:
-        df[strategy] = df['response'].apply(lambda x: x[strategy] if strategy in x else 'undefined')
+        consolidated_df[strategy] = consolidated_df['response'].apply(lambda x: x[strategy] if strategy in x else 'undefined')
+    consolidated_df.drop('response', axis=1, inplace=True)
+    consolidated_df.to_csv(rf'C:\Users\Murilo\Desktop\Projetos\TCC\results\consolidated.csv')
+    return consolidated_df
 
-    print(df.head())
-    df = df[df['label'].isin(labels)]
+def filter_by_model(model, df):
+    return df[df['model'] == model]
 
-    
-    df_temp = df.copy()
+
+def get_rq1_consolidated_result(labels, models ):
+    df = get_data(models)
     report_text = ''
     for strategy in strategys:
-        df_temp['predicted_label'] = df_temp[strategy]
-        df_temp = df_temp[df_temp['predicted_label'].isin(labels)]  
-        df_temp.to_csv(f'{model_name}_{strategy}.csv', index=False)
-        cm = confusion_matrix(df_temp['label'], df_temp['predicted_label'], labels=labels)
-        report = classification_report(df_temp['label'], df_temp['predicted_label'], target_names=labels)
-        report_text += f"Confusion Matrix for {model_name} - {strategy}:\n"
-        report_text += f"{cm}\n"
-        report_text += f"Classification Report for {model_name} - {strategy}:\n"
-        report_text += f"{report}\n"
-        plot_confusion_matrix(cm, report, strategy, labels)
-    return report_text
+        if (strategy != 'zero_shot'):
+            continue
+        for model in models:
+
+            df_temp = filter_by_model(model, df).copy() 
+            df_temp['predicted_label'] = df_temp[strategy]
+            df_temp = df_temp[df_temp['predicted_label'].isin(labels)]
+            df_temp = df_temp.drop(columns=strategys)
+            cm = confusion_matrix(df_temp['label'], df_temp['predicted_label'], labels=labels)
+            report = classification_report(df_temp['label'], df_temp['predicted_label'], labels=labels)
+            report_text += f"Confusion Matrix for {model} - {strategy}:\n"
+            report_text += f"{cm}\n"
+            report_text += f"Classification Report for {model} - {strategy}:\n"
+            report_text += f"{report}\n"
+            #plot_confusion_matrix(cm, report, strategy, labels)
+    with open(rf'C:\Users\Murilo\Desktop\Projetos\TCC\results\report_rq1_consolidated.txt', "w+") as file:
+        file.write(report_text)
+    return 
 
 def main():
     labels = ['sec', 'nonsec']
     models = ['llama3','mistral', 'gemma']
-    report_text = ''
+    get_rq1_consolidated_result(labels, models)
     for model in models:
         path = rf'C:\Users\Murilo\Desktop\Projetos\TCC\results\{model}.json'
-        report_text += confusion_matrix_for_models(path, labels, model)
-        # Save the report to a .txt file
-    with open(rf'C:\Users\Murilo\Desktop\Projetos\TCC\results\report.txt', "w+") as file:
-        file.write(report_text)
-    
 
 if __name__ == "__main__":
     main()
