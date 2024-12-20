93f57b15bda0cac6f1b713ef9e548306bb087f57
Murilo Martin <murilodsmartin@gmail.com>
2024-12-20 09:10:22 -0300

    changes

--- source/conf_matrix.py
+++ source/conf_matrix.py
@@ -5,7 +5,7 @@ import json
 import matplotlib.pyplot as plt
 import seaborn as sns
 from prompt import strategys
-from eda import rq1_eda, rq2_eda, rq2_best_improvement
+from eda import rq1_eda, rq2_eda, rq2_best_improvement, rq2_improvement_by_strategy, rq3_individual,rq3_consolidated
 
 def get_files_names():
     folder_path = 'data'
@@ -53,6 +53,19 @@ def get_data(models):
 def filter_by_model(model, df):
     return df[df['model'] == model]
 
+def filter_by_project_id(id, df):
+    if type(id) == list:
+        return df[df['project_id'].isin(id)]
+    return df[df['project_id'] == id]
+
+def filter_out_projects(ids, df):
+    filtered_rows = []  
+    for _, row in df.iterrows():
+        if row['project_id'] not in ids:
+            filtered_rows.append(row)
+    
+    return pd.DataFrame(filtered_rows)
+
 def get_df_from_report(report):
     report_data = []
     lines = report.split('\n')
@@ -64,12 +77,12 @@ def get_df_from_report(report):
         row['precision'] = float(row_data[1])
         row['recall'] = float(row_data[2])
         row['f1_score'] = float(row_data[3])
-        row['support'] = float(row_data[4])
+        row['support'] = int(row_data[4])
         report_data.append(row)
-    accuracy_line = [line for line in lines if 'accuracy' in line][0]
-    accuracy_value = accuracy_line.split()[-2]
-    for el in report_data:
-        el['accuracy']=accuracy_value
+    # accuracy_line = [line for line in lines if 'accuracy' in line][0]
+    # accuracy_value = accuracy_line.split()[-2]
+    # for el in report_data:
+    #     el['accuracy']=accuracy_value
     
 
     return pd.DataFrame.from_dict(report_data)
@@ -127,18 +140,119 @@ def get_rq2_consolidated_result(labels, models ):
 
     with open(rf'C:\Users\Murilo\Desktop\Projetos\TCC\results\report_rq2_consolidated.txt', "w+") as file:
         file.write(report_text)
-    print(consolidated_results_df)
+    return consolidated_results_df
+
+def get_rq3_individual_secreq_result(labels, models ):
+    project_dict = {
+        50: "CPN",
+        51: "ePurse",
+        52: "GPS"
+    }
+    df = get_data(models)
+    report_text = ''
+    consolidated_results_df = pd.DataFrame()
+    for strategy in strategys:
+        if (strategy == 'zero_shot'):
+            continue
+        for model in models:
+            for projectId in [50,51,52]:
+                df_temp = filter_by_model(model, df).copy() 
+                df_temp = filter_by_project_id(projectId, df_temp).copy()
+                df_temp['predicted_label'] = df_temp[strategy]
+                df_temp = df_temp[df_temp['predicted_label'].isin(labels)]
+                df_temp = df_temp.drop(columns=strategys)
+                cm = confusion_matrix(df_temp['label'], df_temp['predicted_label'], labels=labels)
+                report = classification_report(df_temp['label'], df_temp['predicted_label'], labels=labels)
+                df_aux = get_df_from_report(report)    
+                df_aux['model'] = model 
+                df_aux['strategy'] = strategy     
+                df_aux['project'] = project_dict[projectId]
+                df_aux['project_id'] = projectId
+                consolidated_results_df = pd.concat([consolidated_results_df, df_aux], ignore_index=True)
+                report_text += f"Confusion Matrix for {model} - {strategy} - {project_dict[projectId]}:\n"
+                report_text += f"{cm}\n"
+                report_text += f"Classification Report for {model} - {strategy} - {project_dict[projectId]}:\n"
+                report_text += f"{report}\n"
+                #plot_confusion_matrix(cm, report, strategy, labels)
+
+    with open(rf'C:\Users\Murilo\Desktop\Projetos\TCC\results\report_rq3_secreq.txt', "w+") as file:
+        file.write(report_text)
+    return consolidated_results_df
+
+def get_rq3_consolidated_secreq_result(labels, models ):
+    df = get_data(models)
+    report_text = ''
+    consolidated_results_df = pd.DataFrame()
+    for strategy in strategys:
+        if (strategy == 'zero_shot'):
+            continue
+        for model in models:
+            df_temp = filter_by_model(model, df).copy() 
+            df_temp = filter_by_project_id([50,51,52], df_temp).copy()
+            df_temp['predicted_label'] = df_temp[strategy]
+            df_temp = df_temp[df_temp['predicted_label'].isin(labels)]
+            df_temp = df_temp.drop(columns=strategys)
+            cm = confusion_matrix(df_temp['label'], df_temp['predicted_label'], labels=labels)
+            report = classification_report(df_temp['label'], df_temp['predicted_label'], labels=labels)
+            df_aux = get_df_from_report(report)    
+            df_aux['model'] = model 
+            df_aux['strategy'] = strategy     
+            consolidated_results_df = pd.concat([consolidated_results_df, df_aux], ignore_index=True)
+            report_text += f"Confusion Matrix for {model} - {strategy} :\n"
+            report_text += f"{cm}\n"
+            report_text += f"Classification Report for {model} - {strategy} :\n"
+            report_text += f"{report}\n"
+            #plot_confusion_matrix(cm, report, strategy, labels)
+
+    with open(rf'C:\Users\Murilo\Desktop\Projetos\TCC\results\report_rq3_secreq.txt', "w+") as file:
+        file.write(report_text)
+    return consolidated_results_df
+
+def get_rq3_promise_result(labels, models ):
+
+    df = get_data(models)
+    report_text = ''
+    consolidated_results_df = pd.DataFrame()
+    model = "mistral"
+    strategy = "raw_inst"
+    
+    df_temp = filter_by_model(model, df).copy() 
+    df_temp = filter_out_projects([50,51,52], df_temp).copy()
+    df_temp['predicted_label'] = df_temp[strategy]
+    df_temp = df_temp[df_temp['predicted_label'].isin(labels)]
+    df_temp = df_temp.drop(columns=strategys)
+    cm = confusion_matrix(df_temp['label'], df_temp['predicted_label'], labels=labels)
+    report = classification_report(df_temp['label'], df_temp['predicted_label'], labels=labels)
+    df_aux = get_df_from_report(report)    
+    df_aux['model'] = model 
+    df_aux['strategy'] = strategy     
+    consolidated_results_df = pd.concat([consolidated_results_df, df_aux], ignore_index=True)
+    report_text += f"Confusion Matrix for {model} - {strategy} - 'Promise':\n"
+    report_text += f"{cm}\n"
+    report_text += f"Classification Report for {model} - {strategy} - 'Promise':\n"
+    report_text += f"{report}\n"
+    #plot_confusion_matrix(cm, report, strategy, labels)
+
+    with open(rf'C:\Users\Murilo\Desktop\Projetos\TCC\results\report_rq3_promise.txt', "w+") as file:
+        file.write(report_text)
     return consolidated_results_df
 
 def main():
     labels = ['sec', 'nonsec']
-    models = ['gpt-4o-mini','llama3.2-vision','llama3.1','mistral-small', 'mistral-nemo','gemma2_27b', 'gemma2_9b' ,'llama3','mistral', 'gemma']
+    models = ['gemma', 'gemma2_27b', 'gpt-4o-mini', 'llama3', 'llama3.1', 'llama3.2-vision', 'mistral', 'mistral-nemo', 'mistral-small']
+
     df_rq1 = get_rq1_consolidated_result(labels, models)
     #rq1_eda(df_rq1)
     df_rq2 = get_rq2_consolidated_result(labels, models)
+    #df_rq3_individual_sec_req = get_rq3_individual_secreq_result(labels, models)
+    #df_rq3_consolidated_sec_req = get_rq3_consolidated_secreq_result(labels, models)
+    #df_rq3_promise = get_rq3_promise_result(labels, models)
     #rq2_eda(df_rq2)
-    rq2_best_improvement(df_rq1,df_rq2)
-    
+    #rq2_best_improvement(df_rq1,df_rq2)
+    rq2_improvement_by_strategy(df_rq1,df_rq2)
+    #rq3_individual(df_rq3_individual_sec_req)
+    #rq3_consolidated(df_rq3_consolidated_sec_req)
+    #rq3_individual(df_rq3_individual_sec_req)
 
 if __name__ == "__main__":
     main()
--- source/eda.py
+++ source/eda.py
@@ -2,45 +2,33 @@ import pandas as pd
 from prompt import strategys
 
 def rq1_eda(df: pd.DataFrame):
-    ordered_sec_df = df[df['class'] =='sec'].sort_values(by='accuracy', ascending=False)
-    ordered_nonsec_df = df[df['class'] =='nonsec'].sort_values(by='accuracy', ascending=False)
-
-    df.set_index(['class', 'model'], inplace=True)
+    df = df[df['class'] == 'sec']
+    df.set_index(['model'], inplace=True)
     columns = [col for col in df.columns if col != 'support'] + ['support']
     df = df[columns]
-    df =  df.sort_values(by=['class', 'model'], ascending=False)
-    caption = f"Performance Metrics by model (zero_shot)"
+    caption = f"Acuracy metrics by model in zero-shot approach"
     df['support'] = df['support'].astype(int)
+    df.drop(columns=['class'], inplace=True)
     latex_table = df.to_latex(index=True, caption=caption, label="tab:rq1", 
                              float_format="%.2f")
     
-    latex_table = latex_table.replace("_",r"\_")
+    latex_table = latex_table.replace("_",r"-")
     
-    # print(latex_table)
+    print(latex_table)
 
 
 def rq2_eda(df: pd.DataFrame):
-    df_grouped = df.groupby('strategy')
-    for strategy, group in df_grouped:
-        group.set_index(['class', 'model'], inplace=True)
-        
-        columns = [col for col in group.columns if col != 'support'] + ['support']
-        group = group[columns]
-        
-        group.drop(columns='strategy', inplace=True)
-        group = group.sort_values(by=['class', 'model'], ascending=False)
-        
-        caption = f"Performance Metrics by model ({strategy})"
-        caption = caption.replace('_', r'\_')
+    df = df[df['class'] == 'sec']
+    df.drop(columns=['class'], inplace=True)
+    df.set_index(['strategy', 'model'], inplace=True)
+    desired_order = ['few_shot', 'zero_shot_cot', 'raw_inst']
+    strategy_order = pd.CategoricalDtype(categories=desired_order, ordered=True)
+    df = df.reorder_levels(['strategy', 'model']).sort_index(level='strategy', key=lambda x: x.astype(strategy_order))
+    df['support'] = df['support'].astype(int)
+    latex_table = df.to_latex(index=True, caption="Acurary by strategy and model", label="tab:metrics", float_format="%.2f", na_rep="NA")
+    latex_table = latex_table.replace("_",r"-")
+    print(latex_table)
 
-        group['support'] = group['support'].astype(int)
-
-        latex_table = group.to_latex(index=True, caption=caption, label="tab:metrics", 
-                                      float_format="%.2f", na_rep="NA")
-
-        # print(f'\n\n\nStrategy: {strategy}\n')
-        # print(latex_table)
-        # print("\n")
 
 def rq2_best_improvement(df_1,df_2):
     diff = pd.DataFrame()
@@ -71,7 +59,7 @@ def rq2_best_improvement(df_1,df_2):
                 diff = pd.concat([diff, pd.DataFrame([diff_dict])], ignore_index=True)
     diff_sec = diff[diff['class'] == 'sec']
     diff_sec.sort_values(by='f1 diff', ascending=False, inplace=True)
-    diff_sec = diff_sec.head(5)
+    diff_sec = diff_sec.head(10)
     diff_sec.drop(columns='class', inplace=True)
     latex_diff_sec = diff_sec.to_latex(index=False, caption="Most improved f1-score for class 'sec'",label="tab:sec_improv", float_format="%.2f", na_rep="NA")
     print('latex_diff_sec\n',latex_diff_sec.replace('_', r'\_'))
@@ -84,6 +72,72 @@ def rq2_best_improvement(df_1,df_2):
     print('latex_diff_nonsec\n',latex_diff_nonsec.replace('_', r'\_'))
 
 
+def rq2_improvement_by_strategy(df_1,df_2):
+    diff_few = pd.DataFrame()
+    diff_raw = pd.DataFrame()
+    diff_cot = pd.DataFrame()
+    dfs_list = [diff_few, diff_cot, diff_raw]
+    models = df_1['model'].unique().tolist()
+    df_1[['precision', 'recall', 'f1_score']] = df_1[['precision', 'recall', 'f1_score']].astype(float)
+    df_2[['precision', 'recall', 'f1_score']] = df_2[['precision', 'recall', 'f1_score']].astype(float)
+    df_1 = df_1[df_1['class'] == 'sec']
+    df_2 = df_2[df_2['class'] == 'sec']
+    df_1.drop(columns=['class'], inplace=True)
+    df_2.drop(columns=['class'], inplace=True)
+    for model in models:
+        
+        baseline = df_1[(df_1['model'] == model)]
+        for strategy in strategys:
+            if strategy == 'zero_shot':
+                continue
+            current_result = df_2[(df_2['model'] == model) & (df_2['strategy'] == strategy)]
+            precision_improv = current_result['precision'].iloc[0] - baseline['precision'].iloc[0]
+            recall_improv = current_result['recall'].iloc[0] - baseline['recall'].iloc[0]
+            f1_score_improve = current_result['f1_score'].iloc[0] - baseline['f1_score'].iloc[0]
+            diff_dict = {
+                'model': model,
+                'strategy': strategy,
+                "precision diff": precision_improv,
+                "recall diff": recall_improv,
+                "f1 diff": f1_score_improve,
+            }
+            if strategy == 'few_shot':
+                dfs_list[0] = pd.concat([dfs_list[0], pd.DataFrame([diff_dict])], ignore_index=True)
+            if strategy == 'zero_shot_cot':
+                dfs_list[1] = pd.concat([dfs_list[1], pd.DataFrame([diff_dict])], ignore_index=True)
+            if strategy == 'raw_inst':
+                dfs_list[2] = pd.concat([dfs_list[2], pd.DataFrame([diff_dict])], ignore_index=True)
+
+    df = pd.concat(dfs_list, ignore_index=True)
+    df.sort_values(by='f1 diff', ascending=False, inplace=True)
+    df.set_index(['strategy', 'model'], inplace=True)
+    desired_order = ['few_shot', 'zero_shot_cot', 'raw_inst']
+    strategy_order = pd.CategoricalDtype(categories=desired_order, ordered=True)
+    df = df.reorder_levels(['strategy', 'model']).sort_index(level='strategy', key=lambda x: x.astype(strategy_order))
+    print(df)
+
+    latex_df = df.to_latex(index=True, caption="Difference in accuracy by strategy compared to the baseline approach",label="tab:accuracyDiff", float_format="%.2f", na_rep="NA")
+    print('latex_diff_nonsec\n',latex_df.replace('_', r'-').replace('zero-shot-cot','auto-CoT'))
+
+def rq3_individual(df):
+    df_orig = df.copy()
+    df = df_orig[((df_orig['model'] == 'mistral-nemo') | (df_orig['model'] == 'mistral')) & (df_orig['strategy'] == 'raw_inst')]
+    df = df[df['class'] == 'sec']
+    print(df)
+    df = df[['model','project','precision','recall','f1_score']]
+    df.sort_values(by=['model','project','f1_score'], ascending=False, inplace=True)
+    latex_df = df.to_latex(index=False, caption="",label="tab:literature_comparison", float_format="%.2f", na_rep="NA")
+    print(latex_df)
+    
+    
+
+def rq3_consolidated(df):
+    df_orig = df.copy()
+    print("Top SECReq approaches for for class sec")
+    df = df_orig[df_orig['class'] == 'sec']
+    df.sort_values(by=['f1_score'], ascending=False, inplace=True)
+    print(df.head(10))
+    
 
 
 
